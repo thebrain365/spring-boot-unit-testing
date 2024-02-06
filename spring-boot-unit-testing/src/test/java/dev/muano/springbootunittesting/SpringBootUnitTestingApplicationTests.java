647fe37365775bbda8b2f88152a16dfccbbfc9ce
Muano <muano.m365@gmail.com>
2024-02-06 19:46:16 +0200

    Initialized the Spring Application

---
+++ spring-boot-unit-testing/src/test/java/dev/muano/springbootunittesting/SpringBootUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.muano.springbootunittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
