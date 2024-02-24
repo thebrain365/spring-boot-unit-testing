d817665f2d95c5d555afd45469b7a0f897df02a3
Muano <muano.m365@gmail.com>
2024-02-24 19:33:52 +0200

    Added the ReviewRepo_SaveAll_ReturnSavedReview test and is passing

---
+++ spring-boot-unit-testing/src/test/java/dev/muano/springbootunittesting/repository/ReviewRepoTests.java
@@ -0,0 +1,140 @@
+package dev.muano.springbootunittesting.repository;
+
+import dev.muano.springbootunittesting.models.Pokemon;
+import dev.muano.springbootunittesting.models.Review;
+import dev.muano.springbootunittesting.repos.PokemonRepo;
+import dev.muano.springbootunittesting.repos.ReviewRepo;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ReviewRepoTests {
+
+    @Autowired
+    private ReviewRepo reviewRepo;
+
+    @Test
+    public void ReviewRepo_SaveAll_ReturnSavedReview() {
+
+        //Arrange
+        Review review = Review.builder()
+                .title("title")
+                .content("content")
+                .stars(5)
+                .build();
+
+        //Act
+        Review savedReview = reviewRepo.save(review);
+
+        //Assert
+        Assertions.assertThat(review.getTitle()).isEqualTo(review.getTitle());
+        Assertions.assertThat(review.getContent()).isEqualTo(review.getContent());
+        Assertions.assertThat(review.getStars()).isEqualTo(review.getStars());
+        Assertions.assertThat(review.getId()).isEqualTo(review.getId());
+    }
+
+//    @Test
+//    public void ReviewRepo_GetAll_ReturnMoreThanOneReview() {
+//
+//        //Arrange
+//        Pokemon pokemon = Pokemon.builder()
+//                .name("pikachu")
+//                .type("electric")
+//                .build();
+//
+//        Pokemon pokemon2 = Pokemon.builder()
+//                .name("pikachu")
+//                .type("electric")
+//                .build();
+//
+//        pokemonRepo.save(pokemon);
+//        pokemonRepo.save(pokemon2);
+//
+//        //Act
+//        List<Pokemon> pokemonList = pokemonRepo.findAll();
+//
+//        //Assert
+//        Assertions.assertThat(pokemonList).isNotNull();
+//        Assertions.assertThat(pokemonList.size()).isEqualTo(2);
+//    }
+//
+//    @Test
+//    public void ReviewRepo_FindById_ReturnsReviewById() {
+//        Pokemon pokemon = Pokemon.builder()
+//                .name("pikachu")
+//                .type("electric")
+//                .build();
+//
+//        pokemonRepo.save(pokemon);
+//
+//        Pokemon returnedPokemon = pokemonRepo.findById(pokemon.getId()).get();
+//
+//        Assertions.assertThat(returnedPokemon).isNotNull();
+//    }
+//
+//    @Test
+//    public void ReviewRepo_FindByType_ReturnsReviewByType() {
+//        Pokemon pokemon = Pokemon.builder()
+//                .name("pikachu")
+//                .type("electric")
+//                .build();
+//
+//        pokemonRepo.save(pokemon);
+//
+//        Pokemon returnedPokemon = pokemonRepo.findByType(pokemon.getType()).get();
+//
+//        Assertions.assertThat(returnedPokemon).isNotNull();
+//    }
+//
+//    @Test
+//    public void ReviewRepo_Update_ReturnsUpdatedReview() {
+//        Pokemon pokemon = Pokemon.builder()
+//                .name("pikachu")
+//                .type("electric")
+//                .build();
+//
+//        pokemonRepo.save(pokemon);
+//
+//        Pokemon returnedPokemon = pokemonRepo.findById(pokemon.getId()).get();
+//        returnedPokemon.setName("Raichu");
+//        returnedPokemon.setType("Fire");
+//
+//        Pokemon updatedPokemon = pokemonRepo.save(returnedPokemon);
+//
+//        Assertions.assertThat(returnedPokemon.getName()).isEqualTo("Raichu");
+//        Assertions.assertThat(returnedPokemon.getType()).isEqualTo("Fire");
+//        Assertions.assertThat(returnedPokemon.getId()).isEqualTo(pokemon.getId());
+//    }
+//
+//    @Test
+//    public void ReviewRepo_DeleteById_DeletesReview() {
+//        Pokemon pokemon = Pokemon.builder()
+//                .name("pikachu")
+//                .type("electric")
+//                .build();
+//
+//        pokemonRepo.save(pokemon);
+//
+//        pokemonRepo.deleteById(pokemon.getId());
+//        Optional<Pokemon> returnedPokemon = pokemonRepo.findById(pokemon.getId());
+//
+//        Assertions.assertThat(returnedPokemon).isEmpty();
+//    }
+}
+
+
+
+
+
+
+
+
+
